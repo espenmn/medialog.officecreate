22c008a5fb8bb63349ed1f6cef800b090fc34653
Espen <espen@medialog.no>
2025-10-29 09:30:41 +0100

    Add view: office-doc-view

---
+++ src/medialog/officecreate/tests/test_view_office_doc_view.py
@@ -0,0 +1,51 @@
+# -*- coding: utf-8 -*-
+from medialog.officecreate.testing import MEDIALOG_OFFICECREATE_FUNCTIONAL_TESTING
+from medialog.officecreate.testing import MEDIALOG_OFFICECREATE_INTEGRATION_TESTING
+from medialog.officecreate.views.office_doc_view import IOfficeDocView
+from plone import api
+from plone.app.testing import setRoles
+from plone.app.testing import TEST_USER_ID
+from zope.component import getMultiAdapter
+from zope.interface.interfaces import ComponentLookupError
+
+import unittest
+
+
+class ViewsIntegrationTest(unittest.TestCase):
+
+    layer = MEDIALOG_OFFICECREATE_INTEGRATION_TESTING
+
+    def setUp(self):
+        self.portal = self.layer['portal']
+        setRoles(self.portal, TEST_USER_ID, ['Manager'])
+        api.content.create(self.portal, 'Folder', 'other-folder')
+        api.content.create(self.portal, 'Document', 'front-page')
+
+    def test_office_doc_view_is_registered(self):
+        view = getMultiAdapter(
+            (self.portal['other-folder'], self.portal.REQUEST),
+            name='office-doc-view'
+        )
+        self.assertTrue(IOfficeDocView.providedBy(view))
+
+    def test_office_doc_view_not_matching_interface(self):
+        view_found = True
+        try:
+            view = getMultiAdapter(
+                (self.portal['front-page'], self.portal.REQUEST),
+                name='office-doc-view'
+            )
+        except ComponentLookupError:
+            view_found = False
+        else:
+            view_found = IOfficeDocView.providedBy(view)
+        self.assertFalse(view_found)
+
+
+class ViewsFunctionalTest(unittest.TestCase):
+
+    layer = MEDIALOG_OFFICECREATE_FUNCTIONAL_TESTING
+
+    def setUp(self):
+        self.portal = self.layer['portal']
+        setRoles(self.portal, TEST_USER_ID, ['Manager'])
---
+++ src/medialog/officecreate/views/office_doc_view.py
@@ -0,0 +1,22 @@
+# -*- coding: utf-8 -*-
+
+# from medialog.officecreate import _
+from Products.Five.browser import BrowserView
+from zope.interface import implementer
+from zope.interface import Interface
+
+# from Products.Five.browser.pagetemplatefile import ViewPageTemplateFile
+
+class IOfficeDocView(Interface):
+    """ Marker Interface for IOfficeDocView"""
+
+
+@implementer(IOfficeDocView)
+class OfficeDocView(BrowserView):
+    # If you want to define a template here, please remove the template from
+    # the configure.zcml registration of this view.
+    # template = ViewPageTemplateFile('office_doc_view.pt')
+
+    def __call__(self):
+        # Implement your own actions:
+        return self.index()
