3e2bb2f41a823e05cfc0a75db1ce14f389788787
Espen <espen@medialog.no>
2025-12-18 13:29:45 +0100

    norwegian translations

--- src/medialog/officecreate/viewlets/folder_add_viewlet.py
+++ src/medialog/officecreate/viewlets/folder_add_viewlet.py
@@ -7,25 +7,13 @@ from plone.app.layout.viewlets import ViewletBase
 class FolderAddViewlet(ViewletBase):
 
     def update(self):
-        super().update()
         self.message = self.get_message()
-        request = self.request
-        context = self.context
-        view = self.view
-
-        # This is the same menu used by the toolbar
-        menu = getMultiAdapter(
-            (context, request, view),
-            name="plone_contentmenu_factory"
-        )
-
-        self.add_menu_items = menu.getMenuItems(context, request)
-
+        
     def get_message(self):
         return self.context.absolute_url()
     
-    # def index(self):
-    #     return super(FolderAddViewlet, self).render()
+    def index(self):
+        return super(FolderAddViewlet, self).render()
 
 
 
