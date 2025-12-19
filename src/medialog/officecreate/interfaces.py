8b7c85a819979eb4fb50f98ad1c0b366ea22cb2a
Espen Moe-Nilssen <espen@medialog.no>
2025-12-19 12:41:15 +0100

    change datagridfield schema

--- src/medialog/officecreate/interfaces.py
+++ src/medialog/officecreate/interfaces.py
@@ -3,10 +3,14 @@
 from zope import schema
 from zope.publisher.interfaces.browser import IDefaultBrowserLayer
 from zope.interface import Interface
+from plone.autoform import directives
+from plone.supermodel import model
+ 
+# from Products.CMFPlone.utils import safe_hasattr
 # from plone.app.contenttypes import _
 # _ = MessageFactory('medialog.officecreate')
 
-class INameValueRow(Interface):
+class INameValueRow(model.Schema):
     name  = schema.TextLine(title=u"Name", required=False)
     value = schema.TextLine(title=u"Value", required=False)
     
